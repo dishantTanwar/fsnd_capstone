e149ca04b35c97b40f11b06b9adee60a647b7821
pirateKing <dishanttanwar@gmail.com>
2021-05-22 21:19:04 +0530

    set database_url

--- config.py
+++ config.py
@@ -12,13 +12,13 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 
-# db_setup = {
-#     "database_name_production" : "capstone_db",
-#     "database_name_test" : "capstone_test_db",
-#     "user_name" : "postgres", # default postgres username
-#     "password" : "postgres", # If no password is there then just set value to None
-#     "port" : "localhost:5432" # default postgres port
-# }
+db_setup = {
+    "database_name_production" : "DATABASE_URL",
+    "database_name_test" : "capstone_test_db",
+    "user_name" : "postgres", # default postgres username
+    "password" : "postgres", # If no password is there then just set value to None
+    "port" : "localhost:5432" # default postgres port
+}
 
 bearer_tokens = {
     "actor": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjRiaENmZERWb09CYmVVb1k2bTJyZCJ9.eyJpc3MiOiJodHRwczovL2ZzbmQtbGVhcm4tYXV0aC51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjBhMjM2MWJjYmU2YzEwMDZiNmYyYjZlIiwiYXVkIjoiY2Fwc3RvbmUiLCJpYXQiOjE2MjE2NjM0MjMsImV4cCI6MTYyMTc0OTgyMywiYXpwIjoiYjZkbDZoM29ROVR0Z2xESlZWMThUTHJDenFINU9NOHUiLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImRlbGV0ZTphY3RvcnMiLCJwYXRjaDphY3RvcnMiLCJwb3N0OmFjdG9ycyJdfQ.mSgyMHgON4ll4ElwgfbNxseYkFWz2YP-GiQE_eELT_5ICLtKhro-gSuDeASi7DzFpQ_t2Vrp2vhumNd72QrZ72PNYji5toMg1aEPEb1rGgz18idZ7ofw8G4ZyjTnw5UyNCs_wmF7pKwo8ZkRzA7IkTc0f-LPWxJ0uGBMKSzI3FuMspS0KyDEZKtp3_lxgnezldaMJzdPEzS644d6eBJCEfak99NcJ9VqELAjwWj33ClLVeWTZIiQsSxJRoVXigNL9-jOkIRdGTzX5dFwviQLV82QY5Eo7OUw4Utc5Eq8Y6PLcdWxEcAWC-3vBfnT2nULE_E6Y2gLwgRC3LPmxuzDDg",
